432787c4b13e0d341d639bc1ac39769c4241f133
SebastianOl <sebastian.olsson@raviteq.com>
2018-01-16 15:08:42 +0100

    asdtyitijhgkjgh
    asdfdsfsgh

---
+++ assets/980x140/980x140_edge.js
@@ -0,0 +1,31 @@
+
+(function(compId){var _=null,y=true,n=false,x24='break-word',x2='5.0.0',x4='rgba(0,0,0,0)',g='image',x29='TextCopy',x31='30px',x37='TextCopy4',e14='${text_1Copy2}',x20='34px',x18='0px',x22='FuturaEF-DemiBold, Helvetica',x38='60px',i='none',x35='25px',x3='6.0.0.400',x23='rgba(217,0,0,1.00)',e17='${text_1}',o='opacity',lf='left',x34='TextCopy2',po='center',x32='FuturaEFTU-Book, Helvetica',x30='74px',e13='${img_2}',e15='${text_3}',l='normal',x='text',x21='auto',x27='24px',x26='Text',x1='6.0.0',x12='rgba(237,211,209,1.00)',x19='250px',p='px',x40='TextCopy3',e16='${text_2}';var g9='text-01.svg',g5='background.png',g8='text-03.svg',g6='Movehome_logo1-RGB.svg',g10='img_1.jpg',g11='img_2.jpg',g7='text-02.svg';var s41="<p style=\"margin: 0px;\">{text_field1_4}</p>",s39="<p style=\"margin: 0px;\">​<span style=\"color: rgb(217, 0, 0);\">{text_field1_3}</span></p>",s36="<p style=\"margin: 0px; font-family: FuturaEFTU-Book, Helvetica; font-weight: 400; font-style: normal; text-decoration: none; font-size: 18px; color: rgb(216, 10, 22); background-color: rgba(0, 0, 0, 0); letter-spacing: 0px; text-transform: none; word-spacing: 0px; text-align: center; text-indent: 0px; line-height: 25px;\"><span style=\"font-size: 16px; font-weight: 500;\">{text_field1_6}</span></p>",s28="<p style=\"margin: 0px;\">​{text_field1_2}</p>",s33="<p style=\"margin: 0px;\"><span style=\"font-size: 16px;\">{text_field1_5}</span></p>",s25="<p style=\"margin: 0px;\">​{text_field1_1}</p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=["https://s1.adform.net/banners/scripts/rmb/Adform.DHTML.js"],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'background',t:g,r:['0px','0px','980px','140px','auto','auto'],f:[x4,im+g5,'0px','0px']},{id:'Movehome_logo1-RGB',t:g,r:['881px','0px','99px','91px','auto','auto'],f:[x4,im+g6,'0px','0px']},{id:'text_2',t:g,r:['0px','0px','980px','140px','auto','auto'],o:'0',f:[x4,im+g7,'0px','0px']},{id:'text_3',t:g,r:['981px','0px','980px','140px','auto','auto'],f:[x4,im+g8,'0px','0px']},{id:'text_1',t:g,r:['-981px','0px','980px','140px','auto','auto'],f:[x4,im+g9,'0px','0px']},{id:'text_1Copy2',t:g,r:['980px','0px','980px','140px','auto','auto'],f:[x4,im+g9,'0px','0px']},{id:'img_1',t:g,r:['0px','0px','370px','140px','auto','auto'],f:[x4,im+g10,'0px','0px']},{id:'img_2',t:g,r:['0px','0px','370px','140px','auto','auto'],o:'0',f:[x4,im+g11,'0px','0px']}],style:{'${Stage}':{isStage:true,r:['null','null','980px','140px','auto','auto'],overflow:'hidden',f:[x12]}}},tt:{d:11500,a:y,l:{"loop":3500},data:[["eid241",o,7000,113,"linear",e13,'0.000000','1'],["eid237",lf,10000,1500,"easeInOutElastic",e14,'981px','0px'],["eid221",lf,5000,1500,"easeInOutElastic",e15,'981px','0px'],["eid57",lf,10000,1500,"easeInOutElastic",e15,'0px','-712px'],["eid42",o,2500,1000,"easeInOutElastic",e16,'0.000000','1'],["eid234",lf,5000,1500,"easeInOutElastic",e17,'0px','-981px']]}},"text_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x18,x18,x19,x20,x21,x21],n:[x22,[25,p],x23,l,i,'',x24,l],text:s25,id:x26,ts:['','',x27,'',''],align:po,t:x},{r:[x18,x20,x19,x20,x21,x21],n:[x22,[25,p],x23,l,i,'',x24,l],text:s28,id:x29,ts:['','',x27,'',''],align:po,t:x}],style:{'${symbolSelector}':{r:[_,_,x19,x30]}}},tt:{d:0,a:y,data:[]}},"text_3":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x18,x18,x19,x31,x21,x21],n:[x32,[18,p],x23,l,i,'',x24,l],text:s33,id:x34,ts:['','',x35,'',''],align:po,t:x},{r:[x18,x31,x19,x31,x21,x21],n:[x32,[14,p],x23,l,i,'',x24,l],text:s36,id:x37,ts:['','',x35,'',''],align:po,t:x}],style:{'${symbolSelector}':{r:[_,_,x19,x38]}}},tt:{d:0,a:y,data:[]}},"text_2":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x18,x18,x19,x20,x21,x21],n:[x22,[25,p],x23,l,i,'',x24,l],text:s39,id:x40,ts:['','',x27,'',''],align:po,t:x},{r:[x18,x20,x19,x20,x21,x21],n:[x22,[25,p],x23,l,i,'',x24,l],text:s41,id:x34,ts:['','',x27,'',''],align:po,t:x}],style:{'${symbolSelector}':{r:[_,_,x19,x30]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-12582071");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11500,function(sym,e){var numLoopsHolder=sym.getVariable("numLoops");numLoopsHolder=numLoopsHolder+1;sym.setVariable("numLoops",numLoopsHolder);if(numLoopsHolder<=1){sym.play("loop");}});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Stage}","click",function(sym,e){window.open(dhtml.getVar('clickTAG','http://www.adform.com'),dhtml.getVar('landingPageTarget','_blank'));});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){sym.setVariable("numLoops",0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'text_1-2'
+(function(symbolName){})("text_1");
+//Edge symbol end:'text_1'
+
+//=========================================================
+
+//Edge symbol: 'text_5-6'
+(function(symbolName){})("text_3");
+//Edge symbol end:'text_3'
+
+//=========================================================
+
+//Edge symbol: 'text_1-2_1'
+(function(symbolName){})("text_2");
+//Edge symbol end:'text_2'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-12582071");
